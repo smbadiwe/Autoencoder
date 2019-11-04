f9f0152ad092e6da7aa6b2f62f6681fb56478fe4
Soma Mbadiwe <somasystemsng@gmail.com>
2019-11-04 15:10:55 -0500

    Implemented means to generate samples and different reconstructions

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 from os import path, listdir
 import random
-
+from glob import glob
 import cv2 as cv
 import numpy as np
 import torch
@@ -14,17 +14,28 @@ def imresize(arr, dim_tuple):
     return np.array(Image.fromarray(arr).resize(size=dim_tuple))
 
 
-def main(loss_fn="mse", checkpoint_file='BEST_checkpoint', num_test_samples=10):
+def main(loss_fn="mse", checkpoint_file='BEST_checkpoint', num_test_samples=5):
     """
 
     :param loss_fn: The loss used. Here's it'll be used as sub-directory to get the actual model.
-    :param checkpoint_file: # model checkpoint file name, without directory or extension
+    :param checkpoint_file: # model checkpoint: number or file name without directory or extension
     :param num_test_samples:
     :return:
     """
+    folder = path.join(save_folder, loss_fn)
+    try:
+        checkpoint_file += 0  # test if param is int
+        fs = glob(path.join(folder, f"checkpoint_{checkpoint_file}_*.tar"))
+        if len(fs) == 0:
+            raise FileNotFoundError(f"Chackpoint {checkpoint_file} model file does not exist")
+        if len(fs) > 1:
+            raise FileNotFoundError(f"Chackpoint {checkpoint_file} file is ambiguous. Not sure what you're looking for")
+        checkpoint_file = fs[0]
+    except TypeError:
+        checkpoint_file = path.join(folder, f"{checkpoint_file}.tar")
     print(f'checkpoint: {checkpoint_file}')
     # Load model
-    checkpoint = torch.load(f"{save_folder}/{loss_fn}/{checkpoint_file}.tar")
+    checkpoint = torch.load(checkpoint_file)
     model = checkpoint['model']
     model = model.to(device)
     model.eval()
@@ -36,9 +47,9 @@ def main(loss_fn="mse", checkpoint_file='BEST_checkpoint', num_test_samples=10):
     n_test_img = len(test_images)
     if n_test_img < num_test_samples:
         test_path = 'data/test/'
-        test_images = [path.join(test_path, f) for f in listdir(test_path) if f.endswith('.jpg')]
+        test_images_orig = [path.join(test_path, f) for f in listdir(test_path) if f.endswith('.jpg')]
 
-        samples = random.sample(test_images, num_test_samples - n_test_img)
+        samples = random.sample(test_images_orig, num_test_samples - n_test_img)
         for i, fpath in enumerate(samples):
             # Read images
             img = imread(fpath)
@@ -63,7 +74,7 @@ def main(loss_fn="mse", checkpoint_file='BEST_checkpoint', num_test_samples=10):
     with torch.no_grad():
         preds = model(imgs)
 
-    ck = checkpoint_file.replace("checkpoint", "")
+    ck = path.basename(checkpoint_file).replace("checkpoint", "").replace(".tar", "")
     for i in range(num_test_samples):
         out = preds[i]
         out = out.cpu().numpy()
